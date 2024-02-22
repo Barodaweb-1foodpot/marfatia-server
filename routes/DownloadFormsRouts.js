33fa2ac13253c26eba68a352e551e863e56cee32
Preyash-Thakkar <Preyashthakkar18@gmail.com>
2024-02-22 14:28:46 +0530

    download form changes

--- routes/DownloadFormsRouts.js
+++ routes/DownloadFormsRouts.js
@@ -8,12 +8,23 @@ const {
   getDownloads,
 } = require("../controllers/downloadsFormsController");
 const router = express.Router();
-
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images/downloads/");
+  },
+  filename: (req, file, cb) => {
+    const originalFileName = file.originalname;
+    cb(null, originalFileName);
+    console.log(originalFileName)
+  },
+});
+const upload = multer({ storage: storage });
 router.get("/", getDownloads);
 router.get("/all", getAllIncDel);
-router.post("/", addNewDownload);
+router.post("/", upload.single("filePath"),addNewDownload);
 router.get("/:id", getDownloadById);
-router.put("/:id", updateDownload);
+router.put("/:id",upload.single("filePath"), updateDownload);
 router.delete("/:id", deleteDownload);
 
 module.exports = router;
